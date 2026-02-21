fd82e4b5790de58ff5bac300fa3e60a0b24614ad
rampaa <rampaa@waifuland.com>
2026-02-21 19:13:49 +0300

    Minor

--- Tsukikage/OCR/OwOCR/OwocrWord.cs
+++ Tsukikage/OCR/OwOCR/OwocrWord.cs
@@ -3,7 +3,7 @@ using System.Text.Json.Serialization;
 namespace Tsukikage.OCR.OwOCR;
 
 // ReSharper disable once ClassNeverInstantiated.Global
-internal sealed record class OwocrWord(string Text, OwocrBoundingBox BoundingBox, string? Separator, OwocrSymbol[]? Symbols)
+internal sealed record class OwocrWord(string Text, OwocrBoundingBox BoundingBox, string? Separator, OwocrSymbol[]? Symbols = null)
 {
     [JsonPropertyName("bounding_box")] public OwocrBoundingBox BoundingBox { get; } = BoundingBox;
     [JsonPropertyName("text")] public string Text { get; set; } = Text;
